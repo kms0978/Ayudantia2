f2bcb4cc03831a85811562948f70635368940d50
Alumno <k.millaldeo01@ufromail.cl>
2023-09-19 02:34:54 -0300

    Tercer avance: Se arreglan unos cuantos datos en los métodos para agregar los datos y sus validaciones

--- imc/src/main/java/org/example/IMC.java
+++ imc/src/main/java/org/example/IMC.java
@@ -8,7 +8,6 @@ public class IMC {
     private double peso; // En Kilogramos
     private double estatura; // En metros
 
-
     public IMC(String nombre,double peso,double estatura) {
         this.nombre = nombre;
         this.peso = peso;
@@ -17,33 +16,28 @@ public class IMC {
     public static void main(String[] args) {
         System.out.println("Inicializando");
         IMC kid_1 = new IMC(" ",1.0,1.0 );
-        IMC kid_2 = new IMC(" ",1.0,1.0 );
-        IMC kid_3 = new IMC(" ",1.0,1.0 );
-        kid_1.agregarDatos();
-        kid_2.agregarDatos();
-        kid_3.agregarDatos();
+        kid_1.agregarNombre();
         System.out.println("Nombre: " + kid_1.getNombre());
-        System.out.println("Peso: " + kid_1.getPeso());
-        System.out.println("Estatura: " + kid_1.getEstatura());
 
-
+        //System.out.println("Peso: " + kid_1.getPeso());
+        //System.out.println("Estatura: " + kid_1.getEstatura());
     }
 
     // Métodos --------------------------------------------------------------------
-
-
-    public void agregarDatos(){
+    public void agregarNombre() {
         do {
             System.out.print("Nombre: ");
             nombre = ingresar();
             mayusIngresado(nombre);
-        }while(!validarNombre(nombre));
-
+        } while (!validarNombre(nombre));
+    }
+    public void agregarPeso() {
         do {
             System.out.print("Peso: ");
             peso = ingresarNum();
-        }while(!validarPeso(peso));
-
+        } while (!validarPeso(peso));
+    }
+    public void agregarEstatura{
         do {
             System.out.print("Estatura: ");
             estatura = ingresarNum();
@@ -57,8 +51,32 @@ public class IMC {
 
     public double calcularIMC(){
         double imc = peso/Math.pow(estatura,2);
+        double imcAprox = (double) ((int) (imc * 100.0)) / 100.0;
+        return imcAprox;
+    }
 
-        return;
+    public String categoria() {
+        double imcCalculado = calcularIMC();
+        String nivel = "";
+        if (imcCalculado < 18.5) {
+            nivel = "Bajo Peso";
+        } else if ((imcCalculado >= 18.5) && (imcCalculado < 25.0)) {
+            nivel = "Normal";
+        } else if ((imcCalculado >= 25) && (imcCalculado < 30.0)) {
+            nivel = "Sobrepeso";
+        } else if (imcCalculado >= 30.0) {
+            nivel = "Obesidad";
+        }
+        return nivel;
+    }
+
+    public double promedioPeso(){
+        double promPeso = 0.0;
+        return promPeso;
+    }
+    public double promedioEstatura(){
+        double promEstatura = 0.0;
+        return promEstatura;
     }
 
     // Validaciones ------------------------------------------------------
@@ -66,9 +84,15 @@ public class IMC {
         return !nombre.isEmpty();
     }
     public double validarPeso(double valor){
+        try{
+
+        }catch (){
+
+        }
         return valor;
     }
-    public double validarEstatura(double estatura){
+    public double validarEstatura(double valor){
+
         return estatura;
     }
 

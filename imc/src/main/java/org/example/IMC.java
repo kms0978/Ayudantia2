968a558c55c77d884ca32415f210e91b85804af7
Alumno <k.millaldeo01@ufromail.cl>
2023-09-19 01:10:11 -0300

    Segundo avance del calculo del IMC

--- imc/src/main/java/org/example/IMC.java
+++ imc/src/main/java/org/example/IMC.java
@@ -6,25 +6,31 @@ public class IMC {
     // Atributos
     private String nombre;
     private double peso; // En Kilogramos
-    private double altura; // En metros
+    private double estatura; // En metros
 
 
-    public IMC(String nombre,String p) {
+    public IMC(String nombre,double peso,double estatura) {
         this.nombre = nombre;
-        this.rut = rut;
+        this.peso = peso;
+        this.estatura = estatura;
     }
     public static void main(String[] args) {
         System.out.println("Inicializando");
-        Propietario p1 = new Propietario(" "," ");
-        p1.agregarDatos();
-        System.out.println("Nombre: " + p1.getNombre());
-        System.out.println("RUT: " + p1.getRut());
+        IMC kid_1 = new IMC(" ",1.0,1.0 );
+        IMC kid_2 = new IMC(" ",1.0,1.0 );
+        IMC kid_3 = new IMC(" ",1.0,1.0 );
+        kid_1.agregarDatos();
+        kid_2.agregarDatos();
+        kid_3.agregarDatos();
+        System.out.println("Nombre: " + kid_1.getNombre());
+        System.out.println("Peso: " + kid_1.getPeso());
+        System.out.println("Estatura: " + kid_1.getEstatura());
+
+
     }
 
-    public String ingresar(){
-        Scanner teclado = new Scanner(System.in);
-        return teclado.nextLine();
-    }
+    // Métodos --------------------------------------------------------------------
+
 
     public void agregarDatos(){
         do {
@@ -32,6 +38,16 @@ public class IMC {
             nombre = ingresar();
             mayusIngresado(nombre);
         }while(!validarNombre(nombre));
+
+        do {
+            System.out.print("Peso: ");
+            peso = ingresarNum();
+        }while(!validarPeso(peso));
+
+        do {
+            System.out.print("Estatura: ");
+            estatura = ingresarNum();
+        }while(!validarEstatura(estatura));
     }
 
     public String mayusIngresado(String word){
@@ -39,11 +55,42 @@ public class IMC {
         return word;
     }
 
+    public double calcularIMC(){
+        double imc = peso/Math.pow(estatura,2);
+
+        return;
+    }
+
+    // Validaciones ------------------------------------------------------
     public Boolean validarNombre(String nombre){
         return !nombre.isEmpty();
     }
+    public double validarPeso(double valor){
+        return valor;
+    }
+    public double validarEstatura(double estatura){
+        return estatura;
+    }
 
+    // Métodos para ingresar Datos --------------------------------------------
+    public String ingresar(){
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextLine();
+    }
+    public double ingresarNum(){
+        Scanner teclado = new Scanner(System.in);
+        return teclado.nextDouble();
+    }
+
+    // Métodos get ------------------------------------------------------------
     public String getNombre(){
         return nombre;
     }
+    public double getPeso(){
+        return peso;
+    }
+    public double getEstatura(){
+        return estatura;
+    }
+
 }
